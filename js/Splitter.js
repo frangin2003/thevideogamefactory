90dffab6ca599d7ae5f773caaa93bfa718472d8d
frangin2003 <charlesphilippe.bernard@gmail.com>
2014-01-18 15:26:19 +0100

    Restarting from scratch

--- js/Splitter.js
+++
@@ -1,9 +0,0 @@
-var Splitter = function(idMainDiv) {
-    //This section is similar to the initialize() method from prototypejs.
-    this.idMainDiv = idMainDiv;
-
-    //Adding a method to an object
-    this.split = function () {
-        $('#'+this.idMainDiv).split({orientation:'vertical', position:'50%'});
-    }
-}
