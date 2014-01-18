22ba7fca5c2935d1c495b88234223aed36c50497
frangin2003 <charlesphilippe.bernard@gmail.com>
2014-01-18 01:24:39 +0100

    UI en cours

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    // Init splitter
+    // Init UI
     var mySplitter = new Splitter("id_splitter");
     mySplitter.split();
+    $('#myTab a:first').tab('show');
 
     // Init canvas
     var stage = new createjs.Stage("myCanvas");
@@ -13,4 +14,12 @@ $( document ).ready(function() {
     circle.y = 100;
     stage.addChild(circle);
     stage.update();
+
+    // Init canvas settings
+    $('#id_canvas_height').on('keyup', function(e) {
+        $('#myCanvas').height = $('#id_canvas_height').val();
+    });
+    $('#id_canvas_width').on('keyup', function(e) {
+        $('#myCanvas').width = $('#id_canvas_width').val();
+    });
 });
